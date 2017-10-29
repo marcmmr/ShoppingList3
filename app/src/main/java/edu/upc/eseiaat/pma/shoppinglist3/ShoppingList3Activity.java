f34123153f61cbfa423a3c08c300b8fbc1def858
marcmmr <marcmontserrrat@gmail.com>
2017-10-29 14:05:17 +0100

    3rd commit

--- app/src/main/java/edu/upc/eseiaat/pma/shoppinglist3/ShoppingList3Activity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist3/ShoppingList3Activity.java
@@ -63,11 +63,13 @@ public class ShoppingList3Activity extends AppCompatActivity {
             FileInputStream fis = openFileInput(FILENAME);
             byte [] buffer = new byte [MAX_BYTES];
             int nread = fis.read(buffer);
-            String content = new String(buffer, 0, nread);
-            String [] lines = content.split("\n");
-            for (String line : lines) {
-                String[] parts = line.split(";");
-                itemList.add(new ShoppingItem(parts[0], parts[1].equals("true")));
+            if(nread>0){
+                String content = new String(buffer, 0, nread);
+                String [] lines = content.split("\n");
+                for (String line : lines) {
+                    String[] parts = line.split(";");
+                    itemList.add(new ShoppingItem(parts[0], parts[1].equals("true")));
+                }
             }
             fis.close();
 
@@ -181,10 +183,27 @@ public class ShoppingList3Activity extends AppCompatActivity {
             case R.id.clear_checked:
                 clearChecked();
                 return true;
-
+            case R.id.clear_all:
+                clearAll();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void clearAll() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.confirm);
+        builder.setMessage(R.string.confirm_clear_all);
+        builder.setPositiveButton(R.string.clear_all, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                itemList.clear();
+                adapter.notifyDataSetChanged();
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel, null);
+        builder.create().show();
     }
 
     private void clearChecked() {
